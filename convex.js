80dcd87713502d49683870307a248fc122403852
Muhammad Hussain Javed <k213584@nu.edu.pk>
2023-11-19 19:55:30 +0500

    Script.js name change
    Changes name of script.js to line_segment.js

--- convex.js
+++ convex.js
@@ -11,13 +11,6 @@ function getMousePos(canvas, evt) {
         scaleX = canvas.width / rect.width,    // relationship bitmap vs. element for x
         scaleY = canvas.height / rect.height;  // relationship bitmap vs. element for y
 
-
-    // return [
-    //     [
-    //         (evt.clientX - rect.left) * scaleX,
-    //         (evt.clientY - rect.top) * scaleY
-    //     ]
-    // ]
     return {
         x: (evt.clientX - rect.left) * scaleX,   // scale mouse coordinates after they have
         y: (evt.clientY - rect.top) * scaleY     // been adjusted to be relative to element
