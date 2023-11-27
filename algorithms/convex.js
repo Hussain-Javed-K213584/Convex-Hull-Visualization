55707af23339adb53d4c5726db287522894237f1
Muhammad Hussain Javed <k213584@nu.edu.pk>
2023-11-27 17:52:19 +0500

    meh

--- algorithms/convex.js
+++ algorithms/convex.js
@@ -126,6 +126,36 @@ plotRandomBtn.addEventListener("click", () => {
     });
 });
 
+function cross(o, a, b) {
+    return (a.x - o.x) * (b.y - o.y) - (a.y - o.y) * (b.x - o.x);
+}
+
+function convexHull(points) {
+    points.sort(function(a, b) {
+        return a.x == b.x ? a.y - b.y : a.x - b.x;
+    });
+
+    var lower = [];
+    for (var i = 0; i < points.length; i++) {
+        while (lower.length >= 2 && cross(lower[lower.length - 2], lower[lower.length - 1], points[i]) <= 0) {
+            lower.pop();
+        }
+        lower.push(points[i]);
+    }
+
+    var upper = [];
+    for (var i = points.length - 1; i >= 0; i--) {
+        while (upper.length >= 2 && cross(upper[upper.length - 2], upper[upper.length - 1], points[i]) <= 0) {
+            upper.pop();
+        }
+        upper.push(points[i]);
+    }
+
+    upper.pop();
+    lower.pop();
+    return lower.concat(upper);
+}
+
 
 const X = 0;
 const Y = 1;
